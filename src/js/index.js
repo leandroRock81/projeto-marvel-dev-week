fa75e094f298567c63b46218f3f6bc7626f7be9c
leandroRock81 <lerockadriano@gmail.com>
2022-05-16 20:30:16 -0300

    reparos

---
+++ src/js/index.js
@@ -0,0 +1,24 @@
+const personagens = document.querySelectorAll('.personagem')
+
+personagens.forEach((personagem) => {
+    personagem.addEventListener('mouseenter', () => {
+
+        const idselecionado = personagem.attributes.id.value;
+
+        const personagemselecionado = document.querySelector('.selecionado')
+        personagemselecionado.classList.remove('selecionado')
+
+        personagem.classList.add('selecionado')
+
+        
+        const imagemjogador1 = document.getElementById('personagem-jogador-1');
+
+        imagemjogador1.src = `./src/imagens/${idselecionado}.png`;
+
+        const nomejogador1 = document.getElementById('nome-jogador-1');
+        
+        const nomeselecionado = personagem.getAttribute('data-name')
+        console.log('nomeselecionado' ,nomeselecionado)
+
+    });
+});
